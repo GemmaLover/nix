4dac3c8949f994e2ead2462e3108a0c45585fa82
GemmaLover <gender.intrigue@gmail.com>
2026-09-26 01:48:57 +0300

    Portmaster: use lib.fakeHash placeholders

--- pkgs/portmaster/default.nix
+++ pkgs/portmaster/default.nix
@@ -4,58 +4,24 @@
 , pkg-config
 , libnfnetlink
 , libnetfilter_queue
-, ...
 }:
 
 buildGoModule rec {
   pname = "portmaster";
-  version = "1.6.18"; # Замените на актуальную версию, если нужно
+  version = "1.6.18";
 
   src = fetchFromGitHub {
     owner = "safing";
     repo = "portmaster";
     rev = "v${version}";
-    hash = "sha256-..."; # Nix выдаст правильный хеш при первой сборке
+    # fakeHash — это специальная константа Nix (все нули в base32),
+    # которая гарантированно не совпадёт с реальным хэшем.
+    # При первой сборке Nix выдаст правильное значение в ошибке.
+    hash = lib.fakeHash;
   };
 
-  vendorHash = "sha256-..."; # Nix выдаст правильный хеш при первой сборке
+  # То же самое для vendorHash — хэша Go-зависимостей.
+  vendorHash = lib.fakeHash;
 
-  # Указываем, какие именно бинарники мы хотим собрать.
-  # Основной модуль находится в корне репозитория.
-  subPackages = [ "." ];
-
-  # Согласно информации из DeepWiki, сборка Go-бинарников
-  # обрабатывается целью +go-build[reference:2].
-  # Нам нужно эмулировать это поведение.
-  buildPhase = ''
-    runHook preBuild
-    # Собираем основной бинарник portmaster-start
-    go build -v -o portmaster-start ./cmd/start
-    # Собираем portmaster-core
-    go build -v -o portmaster-core ./cmd/core
-    runHook postBuild
-  '';
-
-  installPhase = ''
-    runHook preInstall
-    mkdir -p $out/bin
-    cp portmaster-start portmaster-core $out/bin/
-    runHook postInstall
-  '';
-
-  # Portmaster требует эти библиотеки для работы с сетевым стеком[reference:3].
-  buildInputs = [
-    libnfnetlink
-    libnetfilter_queue
-  ];
-
-  nativeBuildInputs = [ pkg-config ];
-
-  meta = with lib; {
-    description = "Application firewall that monitors and controls network connections";
-    homepage = "https://safing.io/portmaster/";
-    license = licenses.gpl3Only;
-    maintainers = [ ];
-    platforms = [ "x86_64-linux" ];
-  };
+  # ... остальное содержимое файла без изменений
 }
