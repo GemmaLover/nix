db79959931ef864c4a07ea05cd5dd3f1bc586a18
GemmaLover <gender.intrigue@gmail.com>
2026-09-25 23:49:56 +0300

    amnezia-vpn: disable incompatible patches for 5.0.3.0

--- pkgs/amnezia-vpn/default.nix
+++ pkgs/amnezia-vpn/default.nix
@@ -1,8 +1,5 @@
 { pkgs ? import <nixpkgs> { } }:
 
-# Переопределяем версию amnezia-vpn на 5.0.3.0.
-# Используем overrideAttrs, чтобы сохранить все зависимости
-# и патчи из стандартного пакета nixpkgs.
 pkgs.amnezia-vpn.overrideAttrs (finalAttrs: previousAttrs: rec {
   version = "5.0.3.0";
 
@@ -10,11 +7,11 @@ pkgs.amnezia-vpn.overrideAttrs (finalAttrs: previousAttrs: rec {
     owner = "amnezia-vpn";
     repo = "amnezia-client";
     rev = "5.0.3.0";
-    # Хэш получен командой:
-    #   nix-prefetch-url --unpack \
-    #     https://github.com/amnezia-vpn/amnezia-client/archive/refs/tags/5.0.3.0.tar.gz
-    # Затем сконвертирован в SRI:
-    #   nix hash to-sri --type sha256 <base32>
-    hash = "sha256-3D2GIpu5yMVQwMTwlyGvd/qHuKJB++4udt976ZxY5lw=";
+    hash = "sha256-3D2GIpu5yMVQwMTwlyGvd/qHuKJB++4udt976ZxY5lw="; # хэш из nix hash to-sri
   };
+
+  # Отключаем патчи, которые не подходят к новой версии.
+  # Если после сборки какие-то функции не работают, можно вернуть
+  # отдельные патчи, добавив их выборочно.
+  patches = [ ];
 })
