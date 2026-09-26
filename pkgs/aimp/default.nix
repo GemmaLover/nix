e2c8726ceb0b1659ddf3a50b34e948722c0dbcef
GemmaLover <gender.intrigue@gmail.com>
2026-09-26 03:35:35 +0300

    aimp: fix wrapGAppsHook3 name and libx11 path

--- pkgs/aimp/default.nix
+++ pkgs/aimp/default.nix
@@ -2,7 +2,7 @@
 , stdenv
 , fetchurl
 , autoPatchelfHook
-, wrapGAppsHook
+, wrapGAppsHook3          # было wrapGAppsHook
 , gtk3
 , gdk-pixbuf
 , cairo
@@ -13,28 +13,25 @@
 , libappindicator-gtk3
 , opus-tools
 , hicolor-icon-theme
-, xorg
-, zstd # <-- добавляем zstd для распаковки
+, libx11                  # было xorg.libX11
+, zstd
 }:
 
 stdenv.mkDerivation rec {
   pname = "aimp";
   version = "6.00.3086beta6";
 
-  # Ссылка на .pkg.tar.zst, которую вы предоставили.
   src = fetchurl {
     url = "https://aimp.ru/files/desktop/builds/aimp-6.00.3086beta6-1-x86_64.pkg.tar.zst";
-    hash = lib.fakeHash; # Хеш-заглушка
+    hash = lib.fakeHash;
   };
 
-  # Инструменты для сборки.
   nativeBuildInputs = [
     autoPatchelfHook
-    wrapGAppsHook
-    zstd # <-- нужен для распаковки
+    wrapGAppsHook3        # здесь тоже
+    zstd
   ];
 
-  # Зависимости, которые требует AIMP.
   buildInputs = [
     gtk3
     gdk-pixbuf
@@ -46,33 +43,21 @@ stdenv.mkDerivation rec {
     libappindicator-gtk3
     opus-tools
     hicolor-icon-theme
-    xorg.libX11
+    libx11                # и здесь
   ];
 
-  # Явно указываем, как распаковывать .pkg.tar.zst.
-  # stdenv не умеет это делать по умолчанию.
   unpackPhase = ''
     tar --use-compress-program=${zstd}/bin/unzstd -xf $src
   '';
 
-  # Устанавливаем файлы из архива в $out.
-  # Структура внутри .pkg.tar.zst аналогична другим пакетам: usr/bin, usr/lib и т.д.
   installPhase = ''
     runHook preInstall
-
-    # Копируем всё содержимое архива в $out.
-    # Это включает usr/bin/aimp, usr/share/applications, usr/share/icons и т.д.
+    mkdir -p $out
     cp -r . $out/
-
-    # Создаём симлинк для удобного запуска из командной строки.
     mkdir -p $out/bin
     ln -sf $out/usr/bin/aimp $out/bin/aimp
-
     runHook postInstall
   '';
-
-  # autoPatchelfHook и wrapGAppsHook обычно работают автоматически.
-  # Дополнительные исправления не требуются.
 
   meta = with lib; {
     description = "AIMP audio player (native Linux version)";
